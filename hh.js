8b17b94c20c3c7bf5a8680be098b2e867fab169d
womarazi <44318803+womarazi@users.noreply.github.com>
2020-11-18 17:46:40 +0100

    Update hh.js

--- hh.js
+++ hh.js
@@ -1033,10 +1033,10 @@ function trollFight(isarena = false, istroll = false, isleague = false, isSeason
     let onFightStart = () => { window.location.href = "https://www.hentaiheroes.com/season-arena.html"; }
     let checkFightStart = (totalWait = 0, checkDelay = 1000, callback) => {
       if (totalWait > 10 * 1000) { refreshPage(); return; }
-      const battleStarted = $enemyEgo[0].style.width !== '100%' // $('[battle-step]:visible').length; // totalWait > 5 * 1000
+      const battleStarted = $enemyEgo[0].style.width && $enemyEgo[0].style.width !== '100%' // $('[battle-step]:visible').length; // totalWait > 5 * 1000
       console.log('battleStarted ? ', battleStarted, $enemyEgo[0].style.width, ' totalWait:', totalWait);
-      if (!battleStarted) { setTimeout( () => { checkFightStart(totalWait += checkDelay, checkDelay); }, checkDelay); return; }
-      // callback();  ////////////////////////////////
+      if (!battleStarted) { setTimeout( () => { checkFightStart(totalWait += checkDelay, checkDelay, callback); }, checkDelay); return; }
+      callback();
     }
     checkFightStart(0, 1000, onFightStart);
     return;
