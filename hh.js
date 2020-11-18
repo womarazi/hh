98cea9aa843951083f13466124a213819f4a709a
womarazi <44318803+womarazi@users.noreply.github.com>
2020-11-18 03:03:37 +0100

    Update hh.js

--- hh.js
+++ hh.js
@@ -1027,12 +1027,14 @@ function canUseKoban() {
 function trollFight(isarena = false, istroll = false, isleague = false, isSeason = false) {
   // console.log("trollFight setup");
   if (isSeason) {
-    const $enemyEgo = $('.base_block.battle_user_block.battle_opponent .bar.bar_ego_blue');
+    // const $egoOnLeagueBattle = $('.base_block.battle_user_block.battle_opponent .bar.bar_ego_blue');
+    const $enemyEgo = $('#battle .season_arena_block.battle_opponent .bar-wrap.battle-bar-ego .bar.bar_ego_blue');
+    console.log('$enemyEgo:', $enemyEgo);
     let onFightStart = () => { window.location.href = "https://www.hentaiheroes.com/season-arena.html"; }
     let checkFightStart = (totalWait = 0, checkDelay = 1000, callback) => {
       if (totalWait > 10 * 1000) { refreshPage(); return; }
       const battleStarted = $enemyEgo[0].style.width !== '100%' // $('[battle-step]:visible').length; // totalWait > 5 * 1000
-      console.log('battleStarted ? ', battleStarted, $enemyEgo[0].style.width, ' totalWait:', totalWait); 
+      console.log('battleStarted ? ', battleStarted, $enemyEgo[0].style.width, ' totalWait:', totalWait);
       if (!battleStarted) { setTimeout( () => { checkFightStart(totalWait += checkDelay, checkDelay); }, checkDelay); return; }
       callback();
     }
