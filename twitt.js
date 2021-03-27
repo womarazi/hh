eefcd6c7b1b78b1f14835e208fdc35bc17d277c2
womarazi <44318803+womarazi@users.noreply.github.com>
2021-03-28 00:54:48 +0100

    Update twitt.js

--- twitt.js
+++ twitt.js
@@ -1,8 +1,9 @@
 function shownsfw(){
-    const a = document.querySelectorAll('article [role="button"] div[dir="auto"]');
+    const a = document.querySelectorAll('article [role="button"] div[dir="auto"] > span > span');
     let wholepagensfw = document.querySelector('[data-testid="emptyState"] [role="button"] div[dir="auto"]');
-    wholepagensfw?.click();
-    for(let e of a) {
+    // wholepagensfw?.click();
+    for (let e of a) {
+      if (e.innerText != "View") continue;
       console.log('showing nsfw:', e);
       e.click();
       while(e) {
