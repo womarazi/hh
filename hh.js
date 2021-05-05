46431afaf303bc39a68fe14342ec6624dce895f2
womarazi <44318803+womarazi@users.noreply.github.com>
2021-05-05 15:47:28 +0200

    Update hh.js

--- hh.js
+++ hh.js
@@ -144,12 +144,19 @@ function makeRunButton(size = 30) {
 function getvar(name) { return localStorage.getItem(name); }
 function setvar(name) { return localStorage.setItem(name); }
 function shopitemsetup(container) {
-  container = container || buttonContainer;
-  for (let i = 0; i < 24; i++) {
+  container = container || document.createElement('div');
+  buttonContainer.append(container);
+  container.style.maxWidth = '200px';
+  container.style.flexWrap = 'wrap';
+  container.style.justifyContent = 'space-evenly';
+  for (let i = 1; i <= 16; i++) {
     let btn = makeVarButton(container, '_hhjs_equip-' + i, 40, false);
-    btn.style.backgroundImage = 'url(https://hh2.hh-content.com/pictures/misc/items_icons/' + i + '.png)';
-    btn.style.backgroundRepeat = 'round';
-   
+    btn.style.backgroundImage = 'url(https://hh2.hh-content.com/pictures/misc/items_icons/' + i + ( i === 16 ? '.svg' : '.png') + ')';
+    // btn.style.backgroundRepeat = 'round';
+    btn.style.backgroundSize: '25px';
+    btn.style.backgroundRepeat: 'no-repeat';
+    btn.style.backgroundPosition: 'center';
+    btn.style.border = 'none';
   }
 }
 
