8005c176f1f3eb79dc435972e030cec31f19abb2
womarazi <44318803+womarazi@users.noreply.github.com>
2021-05-15 23:51:47 +0200

    Update img.js

--- img.js
+++ img.js
@@ -2,7 +2,7 @@ function imgmain(){
     if (excludeDomains.indexOf(location.host) >= 0) return;
     const container = document.createElement('div');
     document.body.append(container);
-    container.style.position = 'absolute';
+    container.style.position = 'fixed';
     container.style.display = 'none';
     container.style.zIndex = '100000';
     container.style.width = 'auto';
@@ -13,23 +13,28 @@ function imgmain(){
     container.dataset.position = 'left';
     const img = document.createElement('img');
     container.append(img);
+    let timers = [];
    
     function showImage(e){
+      console.log('show image', e);
       if (e.target.tagName !== 'IMG' || e.target.src === img.src) return;
       container.style.display = 'block';
       img.src = e.target.src;
       for(let timer of timers) clearTimeout(timer);
-    }
-    const timers = [];
-    function hideImageDelay(e) {
-      if (e.target.tagName !== 'IMG' || e.target.src !== img.src) return;
-      timers.push(setTimeout(()=>hideImage(e), 500);
+      timers = [];
     }
     function hideImage(e){
+      console.log('hide image', e);
       if (e.target.tagName !== 'IMG' || e.target.src !== img.src) return;
       container.style.display = 'none';
     }
+    function hideImageDelay(e) {
+      console.log('hide image delay', e);
+      if (e.target.tagName !== 'IMG' || e.target.src !== img.src) return;
+      timers.push(setTimeout(()=>hideImage(e), 500));
+    }
     function moveContainer(){
+      console.log('move container', e);
       if (container.dataset.position === 'left') {
         container.dataset.position = 'right';
         container.style.right = '0';
@@ -39,9 +44,9 @@ function imgmain(){
         container.style.right = '';
         container.style.left = '0'; }
     }
-    $(container).on('click', moveContainer);
-    $(document.body).on('mouseenter', showImage);
-    $(document.body).on('mouseleave', hideImage);
+    container.addEventListener('click', moveContainer);
+    document.body.addEventListener('mouseenter', showImage);
+    document.body.addEventListener('mouseleave', hideImageDelay);
 
 }
 
