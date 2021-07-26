b7d298da1a87e4b3fecc8521713997be87be3304
womarazi <44318803+womarazi@users.noreply.github.com>
2021-07-26 23:09:39 +0200

    Update hh.js

--- hh.js
+++ hh.js
@@ -104,7 +104,9 @@ function seasonmain2021(count = 0, delay = 200) {
     o.fight = getWinRatio2021(you, o);
     console.log('scoringFunction(o.fight.winrate, o.rewards.pt, o.rewards.gxp, o.rewards.aff)', {o, scoringFunction});
     o.score = scoringFunction(o.fight.winrate, o.rewards.pt, o.rewards.gxp, o.rewards.aff);
-    exportDataToGui(o, i);
+    // exportDataToGui(o, i);
+    // huhbhnjmk
+    exportDataToGui(o, i);// uyiujo
   });
   return seasonPlayers;
 }
