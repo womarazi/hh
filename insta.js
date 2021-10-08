5bcc502205d50204b6d19da2528ed3b8c97fa60d
womarazi <44318803+womarazi@users.noreply.github.com>
2021-10-08 23:19:31 +0200

    Update insta.js

--- insta.js
+++ insta.js
@@ -22,13 +22,16 @@ function maxresreplace() {
     }
 }
 
-function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
+
+if (!docReady)
+    window.docReady = function docReady(fn) {
+        // see if DOM is already available
+        if (document.readyState === "complete" || document.readyState === "interactive") {
+            // call on next available tick
+            setTimeout(fn, 1);
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
+        }
     }
-}
+
 docReady(maxressetup);
