42de93bfeb1091d608c5d0d9ddb77aec597bfd14
womarazi <44318803+womarazi@users.noreply.github.com>
2021-11-27 20:13:30 +0100

    Update hh.js

--- hh.js
+++ hh.js
@@ -2092,6 +2092,7 @@ function towerOfFameMain2021(autorefill = true){
     if (mustStopToRefill()) { console.log("must stop to refill"); return; }
     //setUrl('https://www.hentaiheroes.com/battle.html?league_battle=1&id_member=' + user.id);
     setUrl('https://www.hentaiheroes.com/league-battle.html?number_of_battles=1&id_opponent='+user.id);
+    break;
     // let html = $leagues.find('[sorting_id="' + key + '"]')[0];
     // if (!html) { alert('html not found codeline 171'); return; }
     // $(html).trigger('click');
